86a7b8d242eaad6c29f717a0aa3cdc370d3c08d2
mttinor <mttinor@yahoo.com>
2024-02-29 16:30:15 +0330

    dlt comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,6 +19,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  // matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
   matcher: ["/", "/login", "/home", "/dashboard"],
 };
